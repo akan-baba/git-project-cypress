ad77bc6810700dd35a7ece0377890647ebb50788
akan-baba <akan_antia@live.co.uk>
2024-09-20 20:27:13 +0100

    config

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     experimentalRunAllSpecs: true,
+    chromeWebSecurity: false,
+    pageLoadTimeout: 20000,
+    retries:1,
+    env: {
+      commandDelay: 500,
+      
+    }
   },
 });
